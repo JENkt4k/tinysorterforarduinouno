2e1eec4eaf54accf975bb1905414451fb648d79c
James Nelson <james.e.nelson@gmail.com>
2020-02-29 00:41:12 -0600

    lots of updates

---
+++ p5.serialport-master/p5.serialport-master/startserver.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+/**
+ * @fileOverview start p5.serialserver by calling start() function in {@link p5.serialserver.js p5.serialserver.js}
+ *
+ * @author Shawn Van Every
+ * @author Jiwon Shin
+ *
+ * @requires 	p5.serialserver.js
+ */
+
+
+let serialserver = require('./p5.serialserver');
+serialserver.start(8081);
+
+console.log("p5.serialserver is running");
+
+/*const http = require('http');
+const WebSocket = require('ws');
+
+const server = http.createServer(function(req, res) {
+  // Send `index.html` contents.
+});
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(8081);*/
